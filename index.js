c79b2a6b7c9d3fcc30d4013194a298ecd3850901
Sheuli Akter Shathy <sheuliakter85@gmail.com>
2022-11-24 12:49:50 +0600

    database connected successfully

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 // const jwt = require('jsonwebtoken');
 require('dotenv').config();
 // const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
@@ -13,6 +13,29 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7czv3fq.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+    try{
+        const usersCollection = client.db('trustedFurniture').collection('users');
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
+    }
+    finally{
+
+    }
+}
+run().catch(console.log);
 
 app.get('/', async (req, res) => {
     res.send('Trusted Furniture server is running');
