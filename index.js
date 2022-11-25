b8eb20e26481e441d7e3718d14dc3a022a0f4b02
Sheuli Akter Shathy <sheuliakter85@gmail.com>
2022-11-25 10:56:12 +0600

    products get by category

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 // const jwt = require('jsonwebtoken');
 require('dotenv').config();
 // const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
@@ -17,13 +17,14 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7czv3fq.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 async function run(){
     try{
         const categoriesCollection = client.db('trustedFurniture').collection('categories');
+        const productsCollection = client.db('trustedFurniture').collection('products');
         const usersCollection = client.db('trustedFurniture').collection('users');
 
         app.get('/categories', async (req, res) => {
@@ -38,6 +39,34 @@ async function run(){
             const result = await usersCollection.insertOne(user);
             res.send(result);
         });
+
+        app.post('/products',  async (req, res) => {
+            const product = req.body;
+            const result = await productsCollection.insertOne(product);
+            res.send(result);
+        });
+
+        app.get('/categories/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const category = await categoriesCollection.findOne(query);
+            res.send(category);
+        })
+        
+        app.get("/products", async (req, res) => {
+            let query= {}
+            if(req.query.categoryName)
+             query = {
+                categoryName:req.query.categoryName
+             }
+            
+            const products = await productsCollection
+              .find(query)
+              
+              .toArray();
+            res.send(products);
+          });
+
     }
     finally{
 
